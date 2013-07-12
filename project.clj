8a3d8883e1a06aa560be5e9c3a45d47b9c1a9581
Adam Blinkinsop <blinks@acm.org>
2013-07-12 10:45:11 -0700

    Add Overtone.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone "0.8.1"]]
   :main oberon.core)
